dd0f298f97a79055dfa2d5e69b09dca135c7c4d9
SandarvaShakya <sandarvashakya34@gmail.com>
2021-06-16 20:39:17 +0545

    Added how-it-works section and added functionality to arrow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-bar");
+function menu() {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-bar");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("nav-bar--toggle");
-});
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-bar--toggle");
+  });
+}
+
+function scroll() {
+  const arrow = document.querySelector(".arrow");
+  const section = document.querySelector("#scroll-view");
+
+  arrow.addEventListener("click", () => {
+    section.scrollIntoView(true);
+  });
+}
+
+menu();
+scroll();
